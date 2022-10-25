1d02ce14786bc83934ac44b99e3c17b74d5f7f41
rktol <iitsuka@iplab.cs.tsukuba.ac.jp>
2022-10-25 18:56:43 +0900

    がっちゃんこ成功

--- src/App.tsx
+++ src/App.tsx
@@ -7,6 +7,7 @@ import Webcam from 'react-webcam';
 import {css} from '@emotion/css';
 import { Camera } from '@mediapipe/camera_utils';
 import { Holistic, Results } from '@mediapipe/holistic';
+import {detectItem, InitHand,InitiationHands,nodCount} from './utils/nonVerbal';
 
 
 const App: React.VFC = () =>{
@@ -14,6 +15,9 @@ const App: React.VFC = () =>{
   const webcamRef = useRef<Webcam>(null)
   const canvasRef = useRef<HTMLCanvasElement>(null)
   const resultsRef = useRef<any>(null)
+  const [isHands, setIsHands] = React.useState<InitiationHands>({right:1,left:1});
+  let nod = [0,0,0]
+  let count = 0
 
    /**
    * 検出結果（フレーム毎に呼び出される）
@@ -22,6 +26,28 @@ const App: React.VFC = () =>{
 
     const onResults = useCallback((results: Results) => {
       resultsRef.current = results
+
+      // 頷きのためのnod関数
+      if(results.faceLandmarks){
+        nod.push(results.faceLandmarks[1].y)
+        nod.shift()
+      }
+
+      // 非言語情報を読み取るための関数
+      let conHands = isHands
+      // console.log(conHands)
+      if(conHands){
+        count = detectItem(results,count,conHands,nod)
+      }
+      
+      // count -= 0.5
+      
+      if(count < 0){
+        count=0
+      }else if(count > 101){
+        count=100
+      }
+      console.log("現在のスコア："+count)
     },[])
 
     useEffect(() => {
@@ -39,8 +65,7 @@ const App: React.VFC = () =>{
           minDetectionConfidence: 0.5,
           minTrackingConfidence: 0.5
         });
-  
-        holistic.onResults(onResults);
+      holistic.onResults(onResults);
   
       if(typeof webcamRef.current !== 'undefined' && webcamRef.current !== null){
         const camera = new Camera(webcamRef.current.video!,{
@@ -53,6 +78,7 @@ const App: React.VFC = () =>{
         camera.start()
       }
     },[onResults])
+
     const OutputData = () => {
       const results = resultsRef.current as Results
       console.log(results)
@@ -61,6 +87,17 @@ const App: React.VFC = () =>{
       width: 1280,
       height: 720,
       facingMode: 'user'
+    }
+    const InitHands = () => {
+      const results = resultsRef.current as Results
+      setIsHands((prev) => {
+          if(prev){
+              return InitHand(results,prev)
+          }else{
+            return prev
+          }
+      })
+      // console.log("init:"+isHands?.left+","+isHands?.right)
     }
 
   return (
@@ -81,6 +118,9 @@ const App: React.VFC = () =>{
      <div className={styles.buttonContainer}>
         <button className={styles.button} onClick={OutputData}>
             Output Data
+        </button>
+        <button className={styles.button} onClick={InitHands}>
+            Init Hand
         </button>
      </div>
 
